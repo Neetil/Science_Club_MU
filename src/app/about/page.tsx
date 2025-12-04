b7541f227d2ec441b156e985aa8c6a23734f57a8
Neetil <sahuji9662@gmail.com>
2025-12-05 02:07:17 +0530

    error resolved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -235,13 +235,14 @@ function ScrollReveal({
       }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
